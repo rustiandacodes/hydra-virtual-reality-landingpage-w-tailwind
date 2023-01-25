fe7cdab1073d2504c4f790972f0899dae558c983
rustiandacode <rustiandazen09@gmail.com>
2023-01-25 20:51:12 +0700

    added services section

--- dist/script.js
+++ dist/script.js
@@ -1,28 +1,3 @@
-// hamburger
-let hamburger = document.querySelector('#hamburger')
-let navPhone = document.querySelector('nav')
-let navLinks = document.querySelector('#links')
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('hamburger-active')
-  navPhone.classList.toggle('nav-phone')
-  navLinks.classList.toggle('nav-links')
-  navLinks.classList.toggle('hidden')
-})
-
-console.log(navLinks)
-
-// navbar fixed
-window.onscroll = () => {
-  const header = document.querySelector('header')
-  const fixedNav = header.offsetTop
-  if (window.pageYOffset > fixedNav) {
-    header.classList.add('navbar-fixed')
-  } else {
-    header.classList.remove('navbar-fixed')
-  }
-  console.log(window.pageYOffset)
-}
-
 // swiper js
 let swiper = new Swiper('.mySwiper', {
   spaceBetween: 10,
@@ -85,3 +60,28 @@ let swiperslider = new Swiper('.servicesSwiper', {
     },
   },
 })
+
+// hamburger
+let hamburger = document.querySelector('#hamburger')
+let navPhone = document.querySelector('nav')
+let navLinks = document.querySelector('#links')
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('hamburger-active')
+  navPhone.classList.toggle('nav-phone')
+  navLinks.classList.toggle('nav-links')
+  navLinks.classList.toggle('hidden')
+})
+
+console.log(navLinks)
+
+// navbar fixed
+window.onscroll = () => {
+  const header = document.querySelector('header')
+  const fixedNav = header.offsetTop
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed')
+  } else {
+    header.classList.remove('navbar-fixed')
+  }
+  console.log(window.pageYOffset)
+}
