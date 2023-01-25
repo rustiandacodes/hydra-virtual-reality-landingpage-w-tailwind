e2db0c69140ea878f174f67379f90ef58abfcfb6
rustiandacode <rustiandazen09@gmail.com>
2023-01-25 20:47:31 +0700

    added services section

--- dist/script.js
+++ dist/script.js
@@ -52,3 +52,36 @@ let swiper = new Swiper('.mySwiper', {
     },
   },
 })
+
+// swiper js
+let swiperslider = new Swiper('.servicesSwiper', {
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grapCursor: 'true',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    530: {
+      slidesPerView: 2,
+    },
+    800: {
+      slidesPerView: 3,
+    },
+    1020: {
+      slidesPerView: 4,
+    },
+  },
+})
