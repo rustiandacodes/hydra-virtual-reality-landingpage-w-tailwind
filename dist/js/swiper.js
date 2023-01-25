e24707b622af502200b9c4e59bdd537e04bc0b0e
rustiandacode <rustiandazen09@gmail.com>
2023-01-25 20:56:14 +0700

    added services section

--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -61,27 +61,4 @@ let swiperslider = new Swiper('.servicesSwiper', {
   },
 })
 
-// hamburger
-let hamburger = document.querySelector('#hamburger')
-let navPhone = document.querySelector('nav')
-let navLinks = document.querySelector('#links')
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('hamburger-active')
-  navPhone.classList.toggle('nav-phone')
-  navLinks.classList.toggle('nav-links')
-  navLinks.classList.toggle('hidden')
-})
-
-console.log(navLinks)
-
-// navbar fixed
-window.onscroll = () => {
-  const header = document.querySelector('header')
-  const fixedNav = header.offsetTop
-  if (window.pageYOffset > fixedNav) {
-    header.classList.add('navbar-fixed')
-  } else {
-    header.classList.remove('navbar-fixed')
-  }
-  console.log(window.pageYOffset)
-}
+console.log('berhasil')
